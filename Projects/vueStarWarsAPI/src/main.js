688f846ae96812cce3fb557dac15856f9df61322
KrizajZan1 <krizajzan1@gmail.com>
2024-08-09 21:53:00 +0200

    Deleted StarWarsAPI project

--- Projects/vueStarWarsAPI/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import NavBar from './NavBar.vue'
-import CharacterCard from './CharacterCard.vue'
-import axios from 'axios'
-
-createApp(App).mount('#app')
