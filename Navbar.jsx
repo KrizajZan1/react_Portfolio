5bfce60cffeb6bfe83c32089e9285ee29972c6b0
KrizajZan1 <krizajzan1@gmail.com>
2024-08-21 10:59:59 +0200

    url image

--- Navbar.jsx
+++ Navbar.jsx
@@ -12,7 +12,6 @@ export default function Navbar() {
 
   return (
     <div id="header">
-      <img src="Assets/background.png" alt="background" id="header"/>
       <div className="container">
         <nav>
           <img src="Assets/logo.png" className="logo" alt="Logo" />
