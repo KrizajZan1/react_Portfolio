ceb54effdb5e5a2983824ff2bd19a080d01f7424
Žan Križaj <krizajzan1@gmail.com>
2024-09-15 21:37:36 +0200

    added aboutme

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/contact.css";
+import "../styles/Contact.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
