4e2a0b3087ae6cb688e338f477e2bdb4dcd02631
Žan Križaj <krizajzan1@gmail.com>
2024-09-23 18:06:27 +0200

    removed WeatherProject from projects folder and also improved projects design

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,61 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSwipeable } from 'react-swipeable'; // Importing react-swipeable
 import '../styles/projects.css';
 
 export default function Projects() {
+  const [currentProject, setCurrentProject] = useState(0);
+
+  const projects = [
+    {
+      title: 'To-do List',
+      description:
+        'Developed simple To-do list website with HTML, CSS, JavaScript where user can add tasks which are saved with localStorage.',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      image: 'src/assets/todoList.png',
+      github: 'https://github.com/KrizajZan1/todoList',
+      demo: 'src/projects/To-do list/index.html',
+    },
+    {
+      title: 'Calculator',
+      description:
+        'Developed a simple calculator website with HTML, CSS and JavaScript.',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      image: 'src/assets/Calculator.png',
+      github: 'https://github.com/KrizajZan1/Calculator',
+      demo: 'src/projects/calculator/calculator.html',
+    },
+    {
+      title: 'Open Weather API',
+      description:
+        'React project that allows user to know weather information of a city he enters in the search bar.',
+      technologies: ['React', 'API', 'CSS'],
+      image: 'src/assets/WeatherProjekt.PNG',
+      github: 'https://github.com/KrizajZan1/Kaldi_WeatherAssignment',
+      demo: 'http://weather.krizajzan.com/',
+    },
+    {
+      title: 'Vue Star Wars API',
+      description:
+        'Developed website with Vue.js that fetches data from SWAPI and displays it.',
+      technologies: ['Vue.js'],
+      image: 'src/assets/StarWarsAPI.png',
+      github: 'https://github.com/KrizajZan1/vueStarWarsAPI',
+    },
+  ];
+
+  const handlePrevClick = () => {
+    setCurrentProject((prev) => (prev === 0 ? projects.length - 1 : prev - 1));
+  };
+
+  const handleNextClick = () => {
+    setCurrentProject((prev) => (prev === projects.length - 1 ? 0 : prev + 1));
+  };
+
+  // Swipe handlers using react-swipeable
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: handleNextClick,
+    onSwipedRight: handlePrevClick,
+    trackMouse: true,
+  });
+
   return (
-    <section id="projects" className="projects">
-      <h2>Browse My Recent <span>Projects</span></h2>
+    <section id="projects" className="projects" {...swipeHandlers}>
+      <h2>
+        Browse My Recent <span>Projects</span>
+      </h2>
 
       <div className="projects-container">
+        <button className="nav-arrow left-arrow" onClick={handlePrevClick}>
+          &#10094;
+        </button>
+
         <div className="project-card">
-          <img src='src/assets/todoList.png' alt="Project One" className="project-image" />
-          <h3>To-do List</h3>
-          <p>Developed simple To-do list website with HTML, CSS, JavaScript where user can add tasks which are saved with localStorage. You can check the page via link below</p>
-          <div className="project-buttons">
-            <a href='https://github.com/KrizajZan1/todoList' target='blank'>
-              <button className="btn">GitHub</button>
+          <img
+            src={projects[currentProject].image}
+            alt={projects[currentProject].title}
+            className="project-image"
+          />
+
+          <div className="project-info">
+            <h3>{projects[currentProject].title}</h3>
+            <div className="space"></div>
+            <p>{projects[currentProject].description}</p>
+            <div className="space"></div>
+            <ul className="tech-list">
+              {projects[currentProject].technologies.map((tech, index) => (
+                <li key={index}>{tech}</li>
+              ))}
+            </ul>
+            <div className="space"></div>
+            <div className="project-buttons">
+              <a
+                href={projects[currentProject].github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn">GitHub</button>
               </a>
-              <a href='src/projects/To-do list/index.html' target='blank'>
-              <button className="btn">Live Demo</button>
-              </a>
+              {projects[currentProject].demo && (
+                <a
+                  href={projects[currentProject].demo}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="btn">Live Demo</button>
+                </a>
+              )}
+            </div>
           </div>
         </div>
-        <div className="project-card">
-          <img src='src/assets/Calculator.png' alt="Project Two" className="project-image" />
-          <h3>Calculator</h3>
-          <p>Developed a simple calculator website with HTML, CSS and JavaScript. You can check the page via link below</p>
-          <div className="project-buttons">
-            <a href='https://github.com/KrizajZan1/Calculator' target='blank'>
-            <button className="btn">GitHub</button>
-            </a>
-            <a href='src/projects/calculator/calculator.html' target='blank'>
-            <button className="btn">Live Demo</button>
-            </a>
-          </div>
-        </div>
-        <div className="project-card">
-          <img src='src/assets/StarWarsAPI.png' alt="React Project" className="project-image" />
-          <h3>Open Weather API</h3>
-          <p>React project, that allows user to know weather information of a city he enters in the search bar.</p>
-          <div className="project-buttons">
-              <a href='https://github.com/KrizajZan1/Kaldi_WeatherAssignment' target='_blank'>
-              <button className="btn">GitHub</button>
-              </a>
-              <a href='src/projects/WeatherProject/index.html' target='_blank'>
-              <button className="btn">Live Demo</button>
-              </a>
-          </div>
-        </div>
-        <div className="project-card">
-          <img src='src/assets/StarWarsAPI.png' alt="Project Three" className="project-image" />
-          <h3>Vue Star Wars API</h3>
-          <p>Developed website with Vue.js that fetches data from SWAPI and displays it. It also allows user to change the data and it is saved in localStorage. You can check the page via link below</p>
-          <div className="project-buttons">
-          <a href='https://github.com/KrizajZan1/vueStarWarsAPI' target='blank'>
-            <button className="btn">GitHub</button>
-            </a>
-          </div>
-        </div>
+
+        <button className="nav-arrow right-arrow" onClick={handleNextClick}>
+          &#10095;
+        </button>
+      </div>
+
+      <div className="project-indicators">
+        {projects.map((_, index) => (
+          <span
+            key={index}
+            className={`indicator ${index === currentProject ? 'active' : ''}`}
+            onClick={() => setCurrentProject(index)}
+          ></span>
+        ))}
       </div>
     </section>
   );
